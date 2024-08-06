3a3bb084cabe63220ac15bb4c98b2887804d5912
anmedyns <anmedyns@student.42roma.it>
2024-08-06 16:10:14 +0200

    ok

---
+++ main.c
@@ -0,0 +1,8 @@
+
+#include "philosopher.h"
+
+int main(int argc, char **argv)
+{
+	
+}
+
---
+++ philosopher.h
@@ -0,0 +1,10 @@
+#ifndef PHILOSOPHER.H
+# define PILOSOPHER.H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdio.h>
+
+#endif
+
